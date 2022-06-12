acbc77bd06957d5a02d35ffa5925fb09da8d4693
Pedro Farias <phjfg@cesar.org.br>
2022-06-12 16:06:52 +0000

    feat: Create cache in tests

--- code/src/domain/data/usecases/local-save-purchases.spec.ts
+++ code/src/domain/data/usecases/local-save-purchases.spec.ts
@@ -1,5 +1,38 @@
+class LocalSavePurchases {
+    constructor(private readonly cacheStore: CacheStore) { }
+
+    async save(): Promise<void> {
+        this.cacheStore.delete()
+    }
+}
+
+interface CacheStore {
+    deleteCallsCount: number
+
+    delete(): void
+}
+
+
+class CacheStoreSpy implements CacheStore {
+    deleteCallsCount = 0
+
+    delete(): void {
+        this.deleteCallsCount++
+    }
+}
+
 describe('LocalSavePurchases', () => {
-    test('', () => {
-        expect(1).toBe(1)
+    test('Should not delete cache on init', () => {
+        const cacheStore: CacheStore = new CacheStoreSpy()
+        new LocalSavePurchases(cacheStore)
+        expect(cacheStore.deleteCallsCount).toBe(0)
     })
-})
+
+    test('Should delete cache on save', async () => {
+        const cacheStore: CacheStore = new CacheStoreSpy()
+        const sut = new LocalSavePurchases(cacheStore)
+        await sut.save()
+
+        expect(cacheStore.deleteCallsCount).toBe(1)
+    })
+}) 
