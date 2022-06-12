de2040b2f1af026fc75dda664765e9cde19f4e16
Pedro Farias <phjfg@cesar.org.br>
2022-06-12 16:12:51 +0000

    feat: Delete cache with given key

--- code/src/domain/data/usecases/local-save-purchases.spec.ts
+++ code/src/domain/data/usecases/local-save-purchases.spec.ts
@@ -2,22 +2,25 @@ class LocalSavePurchases {
     constructor(private readonly cacheStore: CacheStore) { }
 
     async save(): Promise<void> {
-        this.cacheStore.delete()
+        this.cacheStore.delete("purchases")
     }
 }
 
 interface CacheStore {
     deleteCallsCount: number
+    key: string
 
-    delete(): void
+    delete(key: string): void
 }
 
 
 class CacheStoreSpy implements CacheStore {
     deleteCallsCount = 0
+    key: string = ""
 
-    delete(): void {
+    delete(key: string): void {
         this.deleteCallsCount++
+        this.key = key
     }
 }
 
@@ -48,5 +51,12 @@ describe('LocalSavePurchases', () => {
         await sut.save()
 
         expect(cacheStore.deleteCallsCount).toBe(1)
+    })
+
+    test('Should call delete with correct key', async () => {
+        const { sut, cacheStore } = makeSut()
+        await sut.save()
+
+        expect(cacheStore.key).toBe("purchases")
     })
 }) 
